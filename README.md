4fd04f5126890d2f237b7da8b259e9841d896efb
Ubuntu Jenkins <ubuntu@jenkins>
2015-05-20 13:15:06 +0000

    Show latest build status of an armada branch

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # jenkins-status
 
 A utility to extract useful information from a local Jenkins server: which tags were built, tested and can be deployed.
+
+Usage:
+
+    $ pip install -r requirements
+    $ jenkinsstatus 
--- jenkinsstatus.py
+++ jenkinsstatus.py
@@ -1,31 +1,131 @@
 import click
+from termcolor import colored
+from tabulate import tabulate
+from datetime import datetime
 import json
 import requests
 
 LOCAL_JENKINS_PORT=8080
 PROTOCOL_SUFFIX="api/json?pretty=true"
+DEBUG=False
 
 def endpoint():
     return "http://localhost:%d" % LOCAL_JENKINS_PORT
+
+def dbg(msg):
+    if DEBUG:
+        print msg
 
 def get(url):
     return requests.get(endpoint() + url)
 
 def getjson(path):
-    return json.loads(get(path + "/" + PROTOCOL_SUFFIX).text)
+    j = get(path + "/" + PROTOCOL_SUFFIX).text
+    dbg(j)
+    return json.loads(j)
     
 
 @click.group()
 def cli():
     pass
 
+def all_project_builds(project_name):
+    return getjson("/job/" + project_name)["builds"]
+
+
+def get_build(name, number):
+    return getjson("/job/%s/%d" % (name, number))
+
+
+def get_build_envars(name, number):
+    return getjson("/job/%s/%d/injectedEnvVars" % (name, number))['envMap']
+
+
+def tests_by_hash(test_suite, branch):
+    project = "%s-test-%s" % (branch, test_suite)
+    builds = all_project_builds(project)
+    r = {}
+
+    for b in builds:
+        n = b["number"]
+        envars = get_build_envars(project, n)
+        status = get_build(project, n)
+        if not 'TAG' in envars:
+            continue
+
+        tag = envars['TAG']
+        if tag in r:
+            continue
+        r[tag] = {
+            "env" : envars,
+            "status" : status
+        }
+    return r
+
+def status_rep(status):
+    if status['building']:
+        return "..."
+    if status['result'] == 'SUCCESS':
+        return colored("V", "green")
+    return colored("X", "red")
 
 @cli.command()
 @click.argument("branch")
-def armada(branch):
-    build_job = "%s-build-docker" % branch
+@click.option("--limit", type=click.types.INT, default=20)
+@click.option("--debug", is_flag=True, default=False)
+def armada(branch, debug, limit):
+    global DEBUG
+    DEBUG = debug
+    build_project = "%s-build-docker" % branch
 
-    print getjson("/job/" + build_job)
+    builds = all_project_builds(build_project)
+
+    results = {}
+    euclid_tests = tests_by_hash("euclid", branch)
+    dbg(euclid_tests)
+    integration_tests = tests_by_hash("integration", branch)
+    
+    hashes_seen = set()
+    for b in builds:
+        n = b["number"]
+        dbg("Processing build %d" % n)
+        build_result = get_build(build_project, n)
+        envars = get_build_envars(build_project, n)
+        try:
+            r = {
+                'details' : build_result,
+                'number' : n,
+                'hash' : envars['TAG'],
+                'deployable': False,
+            }
+        except Exception, e:
+            dbg("Error processing build %d: " % n + str(e))
+            continue
+
+        ts = build_result['timestamp']
+        
+        r['time'] = datetime.fromtimestamp(ts / 1000)
+
+        if r['hash'] in hashes_seen:
+            continue
+        hashes_seen.add(r['hash'])
+
+        r['build'] = status_rep(build_result)
+
+        if build_result['result'] == "SUCCESS":
+            r['euclid-test'] = status_rep(euclid_tests[r['hash']]['status'])
+            r['int-test'] = status_rep(integration_tests[r['hash']]['status'])
+
+        results[n] = r
+    headers = ["#", "time", "hash", "build", "euclid-test", "int-test"]
+    rows = []
+
+    for n in reversed(sorted(results.keys())):
+        r = results[n]
+        rows.append([n, r["time"], r["hash"], r["build"], r["euclid-test"], r["int-test"]])
+
+    print tabulate(rows, headers=headers)
+    
 
 
 if __name__ == "__main__":
