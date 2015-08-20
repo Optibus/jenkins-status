95064d979533df73d64e03f4eac46a728904def1
Assaf Lavie <me@assaflavie.com>
2015-08-20 14:37:02 +0300

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jenkinsstatus',
-    version='0.1',
+    version='0.1.1',
     py_modules=['jenkinsstatus'],
     install_requires=[
         'Click',
