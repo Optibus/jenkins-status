22d898c372c68cf284b5f07d15a7cdf80b15dd8b
eitan <eitan@optibus.co.il>
2015-12-29 20:22:38 +0200

    update versio to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jenkinsstatus',
-    version='0.1.4',
+    version='0.1.5',
     py_modules=['jenkinsstatus'],
     install_requires=[
         'Click',
