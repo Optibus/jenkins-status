9c6dd5c2fa76787119cdb2a2c6ae4abdb5820899
Uri <urishalit@gmail.com>
2018-01-21 10:07:31 +0200

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jenkinsstatus',
-    version='0.1.5',
+    version='0.1.6',
     py_modules=['jenkinsstatus'],
     install_requires=[
         'Click',
