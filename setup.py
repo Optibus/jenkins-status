fa685619015aff318458761b6596aa6c19b6d6da
Uri <urishalit@gmail.com>
2018-03-26 17:05:14 +0300

    Change version of jenkins-status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jenkinsstatus',
-    version='0.1.8',
+    version='0.1.9',
     py_modules=['jenkinsstatus'],
     install_requires=[
         'Click',
