29649534977702c1dc12bbc92d666385c7458023
Uri <urishalit@gmail.com>
2018-03-27 09:03:51 +0300

    Add testim suite to status check

--- jenkinsstatus.py
+++ jenkinsstatus.py
@@ -11,7 +11,7 @@ PROTOCOL_SUFFIX = "api/json?pretty=true"
 DEBUG = False
 NO_COLOR = False
 
-TEST_SUITES = ["integration", "euclid", "e2e"]
+TEST_SUITES = ["integration", "euclid", "e2e", "testim"]
 
 
 def endpoint():
